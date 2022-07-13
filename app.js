9b3db15913ad12f24285df5a5c8e53375b6b74dc
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2022-07-13 19:25:00 +0100

    another

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const API_URL = "https://www.greetingsapi.com/random"
         let myObject = await fetch(url);
         let myText = await myObject.json();
         let greeting = myText.greeting;
+        let lang = myText.language;
+        console.log(myText)
         document.getElementById("hello").innerHTML = greeting;
+        document.getElementById("lang").innerHTML = lang;
     }
 
 
